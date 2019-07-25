7b5178b0e2483215e906c1d720ec5c088bc6e085
Nikita Nogai <nikita.nogai@zensoft.io>
2019-07-25 15:40:49 +0600

    js to ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+import {app, BrowserWindow} from 'electron'
+import path from 'path'
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const globals: {mainWindow?: BrowserWindow} = {mainWindow: undefined}
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
+  globals.mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -17,17 +17,17 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  globals.mainWindow.loadFile('static/index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  globals.mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  globals.mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    globals.mainWindow = undefined
   })
 }
 
@@ -46,7 +46,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (globals.mainWindow === undefined) createWindow()
 })
 
 // In this file you can include the rest of your app's specific main process
